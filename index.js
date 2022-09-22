f47e8db484191ca1f6a2951e6f34586165bf5273
William Combee <william.combee@yahoo.com>
2022-09-21 19:36:50 -0500

    added switch to index

--- index.js
+++ index.js
@@ -10,3 +10,72 @@ const Manager = require('./lib/Manager');
 
 // create page
 const generateHTML = require('./src/page-template')
+
+//team member input 
+const teamMembers = [];
+
+function runApp() {
+
+    //team member user input
+    function mainPrompt() {
+        inquirer.prompt([{
+          type: "list",
+          message: "What type of employee do you want to add?",
+          name: "addEmployeePrompt",
+          options: ["Manager", "Engineer", "Intern", "None"]  
+        }]).
+        then(function (userInput) {
+            switch(userInput.addEmplyeePrompt) {
+                case "Manager":
+                    addManager();
+                    break;
+                case "Engineer":
+                    addEngineer();
+                    break;
+                case "Intern":
+                    addIntern();
+                    break;
+                default:
+                    htmlBuilder();    
+            }
+        })
+
+    }
+
+}
+
+function addManager() {
+    inquirer.prompt ([
+
+        {
+            type: "input",
+            name: "managerName",
+            message: "What is the manager's name?"
+          },
+      
+          {
+            type: "input",
+            name: "managerId",
+            message: "What is the manager's employee ID number?"
+          },
+      
+          {
+            type: "input",
+            name: "managerEmail",
+            message: "What is the manager's email?"
+          },
+      
+          {
+            type: "input",
+            name: "managerOfficeNumber",
+            message: "What is the manager's office number?"
+          }
+    ]).
+    then(answers => {
+        const manager = new Manager( answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+        teamMembers.push(manager);
+        mainPrompt();
+    });
+
+}
+
